86f433a5d12dd463e19636b88643b8feee6dd3ac
james hemmen <james.hemmen@ingka.ikea.com>
2024-09-05 15:11:22 -0400

    (PATCH) update license references throughout

--- internal/prom/exporter.go
+++ internal/prom/exporter.go
@@ -1,10 +1,10 @@
 /*
 Copyright © 2024 Ingka Holding B.V. All Rights Reserved.
-Licensed under the GPL, Version 3 (the "License");
+Licensed under the GPL, Version 2 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-       <https://www.gnu.org/licenses/gpl-3.0.en.html>
+       <https://www.gnu.org/licenses/gpl-2.0.en.html>
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
